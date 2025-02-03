f130f99b5f87071d3f00965fefd93421e4d71917
vrykolakas166 <hhtb.bdh@gmail.com>
2025-02-03 18:18:11 +0700

    fix

--- model_training/utils/get_data_frame.py
+++ model_training/utils/get_data_frame.py
@@ -2,9 +2,5 @@ import pandas as pd
 
 
 def get_data_frame(file_url):
-    # df = pd.read_csv(file_url)
-
-    # TESTING: Read the first 10,000 rows
-    df = pd.read_csv(file_url, nrows=15000)
-
+    df = pd.read_csv(file_url)
     return df
